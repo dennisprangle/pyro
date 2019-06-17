7f1a18c8b06ac3cf9d9cd1102617eccf4afbfbb1
Adam Foster <a.evan.foster@gmail.com>
2019-06-17 10:08:19 +0100

    playing with gradient

--- examples/contrib/oed/ubmi.py
+++ examples/contrib/oed/ubmi.py
@@ -3,7 +3,7 @@ from __future__ import absolute_import, division, print_function
 import torch
 
 from pyro.contrib.util import lexpand
-from pyro.contrib.glmm import group_assignment_matrix
+from pyro.contrib.glmm import group_assignment_matrix, analytic_posterior_cov
 
 try:
     from contextlib import ExitStack  # python 3
@@ -13,12 +13,11 @@ except ImportError:
 
 N = 15
 prior_scale_tril = torch.tensor([[10., 0.], [0., 2.]])
-AB_test_designs = torch.stack([group_assignment_matrix(torch.tensor([n, N-n])) for n in torch.linspace(0, N, N+1)])
 
 
 def linear_model(thetas):
     design = lexpand(torch.stack([torch.sin(thetas), torch.cos(thetas)], dim=-1), 1)
-    print(design.abs().sum(1))
+    # print(design.abs().sum(1))
     x_dist = torch.distributions.MultivariateNormal(torch.zeros(2), scale_tril=prior_scale_tril)
     x = x_dist.sample()
     x_prime = x_dist.sample()
@@ -32,21 +31,44 @@ def linear_model(thetas):
     log_prob = y_dist.log_prob(y)
 
     g1 = torch.autograd.grad([log_prob], [y])
+    # print(eps, g1)
 
     g2 = torch.autograd.grad([y], [thetas], grad_outputs=g1, retain_graph=True)[0]
     g3 = torch.autograd.grad([y_prime], [thetas], grad_outputs=g1)[0]
 
-    return g2 - g3
+    return g3
 
 
 def opt_mi():
     thetas = torch.zeros(N, requires_grad=True)
     optimizer = torch.optim.SGD([thetas], lr=0.005)
     for i in range(5000):
-        thetas.grad = linear_model(thetas)
+        thetas.grad = (1/10)*sum(linear_model(thetas) for _ in range(10))
         optimizer.step()
+        design = torch.stack([torch.sin(thetas), torch.cos(thetas)], dim=-1)
+        f = torch.logdet(analytic_posterior_cov(prior_scale_tril ** 2, design, 1.0))
+
+    print(f)
+    print(design.abs().sum(0))
+
+
+def opt_true():
+    thetas = torch.rand(N, requires_grad=True)
+    optimizer = torch.optim.SGD([thetas], lr=0.005)
+    for i in range(5000):
+        optimizer.zero_grad()
+        design = torch.stack([torch.sin(thetas), torch.cos(thetas)], dim=-1)
+        #design.register_hook(print)
+
+        f = torch.logdet(analytic_posterior_cov(prior_scale_tril ** 2, design, 1.0))
+        f.backward()
+        optimizer.step()
+
+    print(f)
+    print(design.abs().sum(0))
 
 
 if __name__ == '__main__':
 
+    opt_true()
     opt_mi()
