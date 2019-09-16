75b28562eea82b81036e858d23d68eabfa81bd8c
ae-foster <adamfosterrules@hotmail.co.uk>
2019-09-16 23:05:31 +0100

    allow ace more resources

--- examples/contrib/oed/ces.py
+++ examples/contrib/oed/ces.py
@@ -270,8 +270,8 @@ def main(num_steps, num_parallel, experiment_name, typs, seed, lengthscale, logl
 
                 elif typ == 'ace-grad':
 
-                    grad_n_samples, grad_n_steps, grad_start_lr, grad_end_lr = 5, 1000, 0.0025, 0.00025
-                    num_grad_acq = 5
+                    grad_n_samples, grad_n_steps, grad_start_lr, grad_end_lr = 7, 800, 0.0025, 0.00025
+                    num_grad_acq = 8
                     posterior_guide = PosteriorGuide((num_parallel, num_grad_acq))
                     posterior_guide.set_prior(rho_concentration, alpha_concentration, slope_mu, slope_sigma)
                     eig_loss = _differentiable_ace_eig_loss(model_learn_xi, posterior_guide, grad_n_samples ** 2,
