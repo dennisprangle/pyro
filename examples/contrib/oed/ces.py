d15ef60748bc751f752e1280438593c354d6363a
ae-foster <adamfosterrules@hotmail.co.uk>
2019-09-16 21:49:47 +0100

    ace should be able to use less samples

--- examples/contrib/oed/ces.py
+++ examples/contrib/oed/ces.py
@@ -270,7 +270,7 @@ def main(num_steps, num_parallel, experiment_name, typs, seed, lengthscale, logl
 
                 elif typ == 'ace-grad':
 
-                    grad_n_samples, grad_n_steps, grad_start_lr, grad_end_lr = 9, 1000, 0.0025, 0.00025
+                    grad_n_samples, grad_n_steps, grad_start_lr, grad_end_lr = 5, 1000, 0.0025, 0.00025
                     num_grad_acq = 5
                     posterior_guide = PosteriorGuide((num_parallel, num_grad_acq))
                     posterior_guide.set_prior(rho_concentration, alpha_concentration, slope_mu, slope_sigma)
