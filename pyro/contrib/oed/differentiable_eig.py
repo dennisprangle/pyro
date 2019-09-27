6391b6805e1d6d56c135a41446c98e86542e7947
Adam Foster <a.evan.foster@gmail.com>
2019-09-27 13:18:23 +0100

    nce add cross term

--- pyro/contrib/oed/differentiable_eig.py
+++ pyro/contrib/oed/differentiable_eig.py
@@ -68,7 +68,7 @@ def differentiable_nce_eig(model, design, observation_labels, target_labels=None
     # Calculate the score parts
     trace.compute_score_parts()
     prescore_function = sum(trace.nodes[l]["score_parts"][1] for l in observation_labels)
-    grad_terms = (terms.detach() - control_variate) * prescore_function
+    grad_terms = (terms.detach() - control_variate) * prescore_function + terms
 
     surrogate_loss = _safe_mean_terms(grad_terms)[0]
     return (surrogate_loss, nce_part)
