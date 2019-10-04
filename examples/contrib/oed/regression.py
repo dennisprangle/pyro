315d767a2eea28699c2dbfd6f292e6bd52d44043
Adam Foster <a.evan.foster@gmail.com>
2019-10-04 10:58:14 +0100

    touch ups

--- examples/contrib/oed/regression.py
+++ examples/contrib/oed/regression.py
@@ -31,80 +31,23 @@ def get_git_revision_hash():
     return subprocess.check_output(['git', 'rev-parse', 'HEAD'])
 
 
-def invsoftplus(x):
-    return (x.exp() - 1).log()
-
-
-def invsigmoid(y):
-    y, y1m = y.clamp(1e-35, 1), (1. - y).clamp(1e-35, 1)
-    return y.log() - y1m.log()
-
-
-def sigmoid(x, top, bottom, ee50, slope):
-    return (top - bottom) * torch.sigmoid((x - ee50) * slope) + bottom
-
-
-def make_docking_model(top_c, bottom_c, ee50_mu, ee50_sigma, slope_mu, slope_sigma, observation_label="y",
-                       xi_init=torch.ones(6)):
-    def docking_model(design_prototype):
-        design = pyro.param("xi", xi_init, constraint=constraints.interval(-100, -1e-6)).expand(design_prototype.shape)
+def make_regression_model(w_loc, w_scale, sigma_scale, xi_init, observation_label="y"):
+    def regression_model(design_prototype):
+        design = pyro.param("xi", xi_init).expand(design_prototype.shape)
         if is_bad(design):
             raise ArithmeticError("bad design, contains nan or inf")
-        batch_shape = design.shape[:-1]
-        with ExitStack() as stack:
-            for plate in iter_plates_to_shape(batch_shape):
-                stack.enter_context(plate)
-            top = pyro.sample("top", dist.Dirichlet(top_c)).select(-1, 0).unsqueeze(-1)
-            bottom = pyro.sample("bottom", dist.Dirichlet(bottom_c)).select(-1, 0).unsqueeze(-1)
-            ee50 = pyro.sample("ee50", dist.Normal(ee50_mu, ee50_sigma)).unsqueeze(-1)
-            slope = pyro.sample("slope", dist.Normal(slope_mu, slope_sigma)).unsqueeze(-1)
-            hit_rate = sigmoid(design, top, bottom, ee50, slope)
-            y = pyro.sample(observation_label, dist.Bernoulli(hit_rate).to_event(1))
+        batch_shape = design.shape[:-2]
+        with pyro.plate_stack("plate_stack", batch_shape):
+            # `w` is shape p, the prior on each component is independent
+            w = pyro.sample("w", dist.Laplace(w_loc, w_scale).to_event(1))
+            # `sigma` is scalar
+            sigma = pyro.sample("sigma", dist.HalfCauchy(sigma_scale))
+            mean = rmv(design, w)
+            sd = sigma * (1 + design.norm(dim=-1))
+            y = pyro.sample(observation_label, dist.Normal(mean, sd).to_event(1))
             return y
 
-    return docking_model
-
-
-# def make_posterior_guide(d, top_prior_c, bottom_prior_c, ee50_prior_mu, ee50_prior_sigma, slope_prior_mu,
-#                          slope_prior_sigma):
-#     def posterior_guide(y_dict, design, observation_labels, target_labels):
-#
-#         y = torch.cat(list(y_dict.values()), dim=-1)
-#
-#         top_mult = pyro.param("A_top", torch.zeros(*d, y.shape[-1]))
-#         top_confidence = pyro.param("top_v", top_prior_c.sum(), constraint=constraints.positive)
-#         top_c = top_confidence * torch.sigmoid(
-#             invsigmoid(top_prior_c[..., 0] / top_prior_c.sum(-1)) + (top_mult * (y - .5)).sum(-1)
-#         )
-#         top_c = torch.stack([top_c, top_confidence - top_c], dim=-1)
-#
-#         bottom_mult = pyro.param("A_ bottom", torch.ones(*d, y.shape[-1]))
-#         bottom_confidence = pyro.param("bottom_v", bottom_prior_c.sum(), constraint=constraints.positive)
-#         bottom_c = bottom_confidence * torch.sigmoid(
-#             invsigmoid(bottom_prior_c[..., 0] / bottom_prior_c.sum(-1)) + (bottom_mult * (y - .5)).sum(-1)
-#         )
-#         bottom_c = torch.stack([bottom_c, bottom_confidence - bottom_c], dim=-1)
-#
-#         ee50_mult = pyro.param("A_ee50", torch.zeros(*d, y.shape[-1]))
-#         ee50_mu = ee50_prior_mu + (ee50_mult * y).sum(-1)
-#         ee50_sigma = pyro.param("ee50_sd", ee50_prior_sigma, constraint=constraints.positive)
-#
-#         slope_mult = pyro.param("A_slope", torch.zeros(*d, y.shape[-1]))
-#         slope_mu = slope_prior_mu + (slope_mult * y).sum(-1)
-#         slope_sigma = pyro.param("slope_sd", slope_prior_sigma, constraint=constraints.positive)
-#
-#         print(ee50_mu)
-#
-#         batch_shape = design.shape[:-1]
-#         with ExitStack() as stack:
-#             for plate in iter_plates_to_shape(batch_shape):
-#                 stack.enter_context(plate)
-#             pyro.sample("top", dist.Dirichlet(top_c))
-#             pyro.sample("bottom", dist.Dirichlet(bottom_c))
-#             pyro.sample("ee50", dist.Normal(ee50_mu, ee50_sigma))
-#             pyro.sample("slope", dist.Normal(slope_mu, slope_sigma))
-#
-#     return posterior_guide
+    return regression_model
 
 
 class TensorLinear(nn.Module):
@@ -135,20 +78,14 @@ class TensorLinear(nn.Module):
 
 
 class PosteriorGuide(nn.Module):
-    def __init__(self, y_dim, batching):
+    def __init__(self, y_dim, w_dim, batching):
         super(PosteriorGuide, self).__init__()
         n_hidden = 64
         self.linear1 = TensorLinear(*batching, y_dim, n_hidden)
         self.linear2 = TensorLinear(*batching, n_hidden, n_hidden)
-        self.output_layer = TensorLinear(*batching, n_hidden, 2 + 2 + 2 + 2)
+        self.output_layer = TensorLinear(*batching, n_hidden, w_dim + 2)
         self.softplus = nn.Softplus()
         self.relu = nn.ReLU()
-
-    def set_prior(self, rho_concentration, alpha_concentration, slope_mu, slope_sigma):
-        self.prior_rho_concentration = rho_concentration
-        self.prior_alpha_concentration = alpha_concentration
-        self.prior_slope_mu = slope_mu
-        self.prior_slope_sigma = slope_sigma
 
     def forward(self, y_dict, design_prototype, observation_labels, target_labels):
         y = y_dict["y"] - .5
@@ -156,23 +93,21 @@ class PosteriorGuide(nn.Module):
         x = self.relu(self.linear2(x))
         final = self.output_layer(x)
 
-        top_c = self.softplus(final[..., 0:2])
-        bottom_c = self.softplus(final[..., 2:4])
-        ee50_mu = final[..., 4]
-        ee50_sigma = self.softplus(final[..., 5])
-        slope_mu = final[..., 6]
-        slope_sigma = self.softplus(final[..., 7])
+        posterior_mean = final[..., :-2]
+        gamma_concentration = self.softplus(final[..., -2])
+        gamma_scale = self.softplus(final[..., -1])
 
         pyro.module("posterior_guide", self)
 
-        batch_shape = design_prototype.shape[:-1]
-        with ExitStack() as stack:
-            for plate in iter_plates_to_shape(batch_shape):
-                stack.enter_context(plate)
-            pyro.sample("top", dist.Dirichlet(top_c))
-            pyro.sample("bottom", dist.Dirichlet(bottom_c))
-            pyro.sample("ee50", dist.Normal(ee50_mu, ee50_sigma))
-            pyro.sample("slope", dist.Normal(slope_mu, slope_sigma))
+        covariance_shape = posterior_mean.shape + (posterior_mean.shape[-1],)
+        posterior_scale_tril = pyro.param("posterior_scale_tril",
+                                          torch.eye(posterior_mean.shape[-1]).expand(covariance_shape),
+                                          constraint=constraints.lower_cholesky)
+
+        batch_shape = design_prototype.shape[:-2]
+        with pyro.plate_stack("guide_plate_stack", batch_shape):
+            pyro.sample("w", dist.MultivariateNormal(posterior_mean, scale_tril=posterior_scale_tril))
+            pyro.sample("sigma", dist.Gamma(gamma_concentration, gamma_scale))
 
 
 def neg_loss(loss):
@@ -181,12 +116,12 @@ def neg_loss(loss):
     return new_loss
 
 
-def opt_eig_loss_w_history(design, loss_fn, num_samples, num_steps, optim, lower, upper, n_high_acc, h_freq):
+def opt_eig_loss_w_history(design, loss_fn, num_samples, num_steps, optim):
 
     params = None
     est_loss_history = []
-    lower_history = []
-    upper_history = []
+    # lower_history = []
+    # upper_history = []
     xi_history = []
     baseline = 0.
     t = time.time()
@@ -203,34 +138,35 @@ def opt_eig_loss_w_history(design, loss_fn, num_samples, num_steps, optim, lower
             raise ArithmeticError("Encountered NaN loss in opt_eig_ape_loss")
         agg_loss.backward(retain_graph=True)
         est_loss_history.append(loss.detach())
+        wall_times.append(time.time() - t)
         optim(params)
         optim.step()
         print(pyro.param("xi").squeeze())
         print('eig', baseline.squeeze())
 
-        if step % h_freq == 0:
-            low = lower(design, n_high_acc, evaluation=True)
-            up = upper(design, n_high_acc, evaluation=True)
-            if isinstance(low, tuple):
-                low = low[1]
-            if isinstance(up, tuple):
-                up = up[1]
-            lower_history.append(low.detach())
-            upper_history.append(up.detach())
-            wall_times.append(time.time() - t)
+        # if step % h_freq == 0:
+        #     low = lower(design, n_high_acc, evaluation=True)
+        #     up = upper(design, n_high_acc, evaluation=True)
+        #     if isinstance(low, tuple):
+        #         low = low[1]
+        #     if isinstance(up, tuple):
+        #         up = up[1]
+        #     lower_history.append(low.detach())
+        #     upper_history.append(up.detach())
+        #     wall_times.append(time.time() - t)
 
     xi_history.append(pyro.param('xi').detach().clone())
 
     est_loss_history = torch.stack(est_loss_history)
     xi_history = torch.stack(xi_history)
-    lower_history = torch.stack(lower_history)
-    upper_history = torch.stack(upper_history)
+    # lower_history = torch.stack(lower_history)
+    # upper_history = torch.stack(upper_history)
     wall_times = torch.tensor(wall_times)
 
-    return xi_history, est_loss_history, lower_history, upper_history, wall_times
+    return xi_history, est_loss_history, wall_times  # , lower_history, upper_history, wall_times
 
 
-def main(num_steps, high_acc_freq, num_samples, experiment_name, estimators, seed, num_parallel, start_lr, end_lr,
+def main(num_steps, num_samples, experiment_name, estimators, seed, num_parallel, start_lr, end_lr,
          device):
     output_dir = "./run_outputs/gradinfo/"
     if not experiment_name:
@@ -248,31 +184,27 @@ def main(num_steps, high_acc_freq, num_samples, experiment_name, estimators, see
             seed = int(torch.rand(tuple()) * 2 ** 30)
             pyro.set_rng_seed(seed)
 
-        D = 100
-        xi_init = lexpand(torch.linspace(-70., -30, D, device=device), num_parallel)
+        n, p = 20, 30
+        xi_init = torch.randn((num_parallel, n, p))
         # Change the prior distribution here
         # prior params
-        top_prior_concentration = torch.tensor([25., 75.], device=device)
-        bottom_prior_concentration = torch.tensor([4., 96.], device=device)
-        ee50_prior_mu, ee50_prior_sd = torch.tensor(-50., device=device), torch.tensor(15., device=device)
-        slope_prior_mu, slope_prior_sd = torch.tensor(-0.15, device=device), torch.tensor(0.1, device=device)
+        w_prior_loc = torch.zeros(p, device=device)
+        w_prior_scale = torch.ones(p, device=device)
+        sigma_prior_scale = torch.tensor(1., device=device)
 
-        model_learn_xi = make_docking_model(
-            top_prior_concentration, bottom_prior_concentration, ee50_prior_mu, ee50_prior_sd, slope_prior_mu,
-            slope_prior_sd, xi_init=xi_init)
+        model_learn_xi = make_regression_model(
+            w_prior_loc, w_prior_scale, sigma_prior_scale, xi_init)
 
         contrastive_samples = num_samples
 
         # Fix correct loss
-        targets = ["top", "bottom", "ee50", "slope"]
-        print("Prior entropy", mean_field_entropy(model_learn_xi, [torch.zeros(num_parallel, D, device=device)],
-                                                  whitelist=targets))
+        targets = ["w", "sigma"]
         if estimator == 'posterior':
             m_final = 20
-            guide = PosteriorGuide(D, (num_parallel,)).to(device)
+            guide = PosteriorGuide(n, p, (num_parallel,)).to(device)
             loss = _differentiable_posterior_loss(model_learn_xi, guide, ["y"], targets)
-            high_acc = loss
-            upper_loss = lambda d, N, **kwargs: vnmc_eig(model_learn_xi, d, "y", targets, (N, int(math.sqrt(N))), 0, guide, None)
+            # high_acc = loss
+            # upper_loss = lambda d, N, **kwargs: vnmc_eig(model_learn_xi, d, "y", targets, (N, int(math.sqrt(N))), 0, guide, None)
 
         elif estimator == 'nce':
             m_final = 40
@@ -280,21 +212,21 @@ def main(num_steps, high_acc_freq, num_samples, experiment_name, estimators, see
                 model=model_learn_xi, design=d, observation_labels=["y"], target_labels=targets,
                 N=N, M=contrastive_samples, **kwargs)
             loss = neg_loss(eig_loss)
-            high_acc = lambda d, N, **kwargs: nce_eig(
-                model=model_learn_xi, design=d, observation_labels=["y"], target_labels=targets,
-                N=N, M=int(math.sqrt(N)), **kwargs)
-            upper_loss = lambda d, N, **kwargs: nmc_eig(
-                model=model_learn_xi, design=d, observation_labels=["y"], target_labels=targets,
-                N=N, M=int(math.sqrt(N)), **kwargs)
+            # high_acc = lambda d, N, **kwargs: nce_eig(
+            #     model=model_learn_xi, design=d, observation_labels=["y"], target_labels=targets,
+            #     N=N, M=int(math.sqrt(N)), **kwargs)
+            # upper_loss = lambda d, N, **kwargs: nmc_eig(
+            #     model=model_learn_xi, design=d, observation_labels=["y"], target_labels=targets,
+            #     N=N, M=int(math.sqrt(N)), **kwargs)
 
         elif estimator == 'ace':
             m_final = 20
-            guide = PosteriorGuide(D, (num_parallel,)).to(device)
+            guide = PosteriorGuide(n, p, (num_parallel,)).to(device)
             eig_loss = _differentiable_ace_eig_loss(model_learn_xi, guide, contrastive_samples, ["y"],
                                                     ["top", "bottom", "ee50", "slope"])
             loss = neg_loss(eig_loss)
-            high_acc = _ace_eig_loss(model_learn_xi, guide, m_final, "y", targets)
-            upper_loss = lambda d, N, **kwargs: vnmc_eig(model_learn_xi, d, "y", targets, (N, int(math.sqrt(N))), 0, guide, None)
+            # high_acc = _ace_eig_loss(model_learn_xi, guide, m_final, "y", targets)
+            # upper_loss = lambda d, N, **kwargs: vnmc_eig(model_learn_xi, d, "y", targets, (N, int(math.sqrt(N))), 0, guide, None)
 
         else:
             raise ValueError("Unexpected estimator")
@@ -303,15 +235,14 @@ def main(num_steps, high_acc_freq, num_samples, experiment_name, estimators, see
         scheduler = pyro.optim.ExponentialLR({'optimizer': torch.optim.Adam, 'optim_args': {'lr': start_lr},
                                               'gamma': gamma})
 
-        design_prototype = torch.zeros(num_parallel, D, device=device)  # this is annoying, code needs refactor
+        design_prototype = torch.zeros(num_parallel, n, p, device=device)  # this is annoying, code needs refactor
 
-        xi_history, est_loss_history, lower_history, upper_history, wall_times = opt_eig_loss_w_history(
-            design_prototype, loss, num_samples=num_samples, num_steps=num_steps, optim=scheduler, lower=high_acc,
-            upper=upper_loss, n_high_acc=m_final**2, h_freq=high_acc_freq)
+        xi_history, est_loss_history, wall_times = opt_eig_loss_w_history(
+            design_prototype, loss, num_samples=num_samples, num_steps=num_steps, optim=scheduler)
 
         if estimator == 'posterior':
             est_eig_history = _eig_from_ape(model_learn_xi, design_prototype, targets, est_loss_history, True, {})
-            lower_history = _eig_from_ape(model_learn_xi, design_prototype, targets, lower_history, True, {})
+            # lower_history = _eig_from_ape(model_learn_xi, design_prototype, targets, lower_history, True, {})
 
         elif estimator in ['nce', 'nce-proposal', 'ace']:
             est_eig_history = -est_loss_history
@@ -320,7 +251,7 @@ def main(num_steps, high_acc_freq, num_samples, experiment_name, estimators, see
 
         results = {'estimator': estimator, 'git-hash': get_git_revision_hash(), 'seed': seed,
                    'xi_history': xi_history.cpu(), 'est_eig_history': est_eig_history.cpu(),
-                   'lower_history': lower_history.cpu(), 'upper_history': upper_history.cpu(),
+                   # 'lower_history': lower_history.cpu(), 'upper_history': upper_history.cpu(),
                    'wall_times': wall_times.cpu()}
 
         with open(results_file, 'wb') as f:
@@ -330,7 +261,7 @@ def main(num_steps, high_acc_freq, num_samples, experiment_name, estimators, see
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Gradient-based design optimization (one shot) with a linear model")
     parser.add_argument("--num-steps", default=500000, type=int)
-    parser.add_argument("--high-acc-freq", default=50000, type=int)
+    # parser.add_argument("--high-acc-freq", default=50000, type=int)
     parser.add_argument("--num-samples", default=10, type=int)
     parser.add_argument("--num-parallel", default=10, type=int)
     parser.add_argument("--name", default="", type=str)
@@ -340,5 +271,5 @@ if __name__ == "__main__":
     parser.add_argument("--end-lr", default=0.001, type=float)
     parser.add_argument("--device", default="cuda:0", type=str)
     args = parser.parse_args()
-    main(args.num_steps, args.high_acc_freq, args.num_samples, args.name, args.estimator, args.seed, args.num_parallel,
+    main(args.num_steps, args.num_samples, args.name, args.estimator, args.seed, args.num_parallel,
          args.start_lr, args.end_lr, args.device)
