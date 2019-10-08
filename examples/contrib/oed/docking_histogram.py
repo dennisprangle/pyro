a44d021b3114b3c3e7314345181e22f6d3ada247
Adam Foster <a.evan.foster@gmail.com>
2019-10-08 18:48:28 +0100

    change histogram script

--- examples/contrib/oed/docking_histogram.py
+++ examples/contrib/oed/docking_histogram.py
@@ -14,7 +14,7 @@ def main():
     designs = {}
     upper = {}
     lower = {}
-    for name in ['docking-ace4', 'docking-baseline']:
+    for name in ['docking-75-ace', 'docking-baseline']:
         fname = output_dir + name + ".pickle"
         with open(fname, 'rb') as f:
             results = pickle.load(f)
@@ -23,27 +23,28 @@ def main():
             upper[name] = results['final_upper_bound'][0]
             lower[name] = results['final_lower_bound'][0]
 
-    designs['docking-ace4'] = designs['docking-ace4'][3,...]
+    designs['docking-75-ace'] = designs['docking-75-ace'][0,...]
     designs['docking-baseline'] = designs['docking-baseline'][0,...]
 
-    d1 = designs['docking-ace4'].view(-1).numpy()
+    d1 = designs['docking-75-ace'].view(-1).numpy()
     d2 = designs['docking-baseline'].view(-1).numpy()
 
-    bins = np.histogram(np.hstack((d1, d2)), bins=30)[1]  # get the bin edges
+    bins = np.histogram(np.hstack((d1, d2)), bins=24)[1]  # get the bin edges
 
+    cmap=plt.get_cmap("Paired")
     fig, (ax1, ax2) = plt.subplots(nrows=2, sharex=True, sharey=True, figsize=(5,4))
-    ax1.hist(d1, bins=bins)
-    ax2.hist(d2, bins=bins)
+    ax1.hist(d1, bins=bins, color=cmap(0))
+    ax2.hist(d2, bins=bins, color=cmap(0))
 
-    ax2.set_xlabel("Predicted binding affinity", fontsize=16)
+    ax2.set_xlabel("Docking score", fontsize=16)
     ax1.set_ylabel("#compounds", fontsize=14)
     ax2.set_ylabel("#compounds", fontsize=14)
     ax1.tick_params(labelsize=14)
     ax2.tick_params(labelsize=14)
     # ax1.set_yticks(fontsize=16)
     # ax2.set_yticks(fontsize=16)
-    ax1.text(-97., 13., "ACE", ha='center', fontsize=16)
-    ax2.text(-93., 13., "Expert", ha='center', fontsize=16)
+    ax1.text(-67., 13., "ACE", ha='center', fontsize=16)
+    ax2.text(-63., 13., "Expert", ha='center', fontsize=16)
     plt.show()
 
 
